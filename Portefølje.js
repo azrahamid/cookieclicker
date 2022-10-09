5b6e1302d67d185a28f11fc180a3c9abf2a9a098
azrahamid <111957781+azrahamid@users.noreply.github.com>
2022-10-09 22:39:25 +0200

    Add files via upload

---
+++ Portefølje.js
@@ -0,0 +1,13 @@
+var clicks = 0;
+function increment (){
+    clicks = clicks + 1;
+    counter.innerHTML = clicks;
+
+    function reset() {
+        clicks = 0;
+        counter.innerHTML = clicks;
+
+    }
+}
+
+
